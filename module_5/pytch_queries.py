40919c4fcae9f61c67a5ca8799033dcc6c85dd65
Allison Partch <apartch623@gmail.com>
2023-01-15 18:03:34 -0700

    pytech_queries commit

--- module_5/pytch_queries.py
+++ module_5/pytch_queries.py
@@ -1,1 +1,19 @@
+import pymongo
+from pymongo import MongoClient
+url = "mongodb+srv://admin:admin@cluster0.yn0tfol.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(url)
+db = client.pytech
+students = db.students
 
+student_list = students.find({})
+
+print("\n -- DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY --")
+for doc in student_list:
+    print(" Student ID: " + doc["student_id"] + "\n First Name: " + doc["first_name"] + "\n Last Name: " + doc["last_name"] + "\n")
+ 
+
+john = students.find_one({"student_id": "1007"})
+print("\n -- DISPLAYING STUDENT DOCUMENT FROM find_one() QUERY --")
+print(" Student ID: " + john["student_id"] + "\n First Name: " + john["first_name"] + "\n Last Name: " + john["last_name"] + "\n")
+
+input("\n\n End of program, press any key to continue...")
