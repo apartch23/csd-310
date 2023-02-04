350c2caf554709278283219d1537f7d0f00ac85f
Allison Partch <apartch623@gmail.com>
2023-02-04 14:13:07 -0700

    mysql_test.py

--- module_7/mysql_test.py
+++ module_7/mysql_test.py
@@ -1,1 +1,39 @@
+# Title: pysports_join queries.py
+#Author: Allison Partch
+#Date: 04 February 2023
+#Test program
 
+#import statements
+import mysql.connector
+from mysql.connector import errorcode
+
+#config database
+config = {
+    "user": "pysports_user",
+    "password": "Titan312",
+    "host": "127.0.0.1",
+    "database": "pysports",
+    "raise_on_warnings": True
+}
+
+# try/catch database errors
+try:
+    db = mysql.connector.connect(**config)
+    print("\n Database user {} connected to MySQL on host {} with database {}".format(config["user"], config["host"], config["database"]))
+    input("\n\n Press any key to continue...")
+
+except mysql.connector.Error as err:
+    """ on error code """   
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print(" The supplied username or password are invalid")
+
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print(" The specified database does not exist")
+
+    else:
+        print(err)
+
+finally:
+        """ close the connection to MySQL"""
+
+        db.close()         
